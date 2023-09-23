bcf427c85fcaba1db62cb4f835300f4dbfa5a313
wuwuarandhg <1610361960@qq.com>
2023-09-23 21:25:26 +0800

    已修改标题和知识库

--- next.config.js
+++ next.config.js
@@ -3,18 +3,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 })
 
 
-//知识机器人
-function embedFastGPT() {
-  var script = document.createElement('script');
-  script.src = "https://fastgpt.run/js/iframe.js";
-  script.id = "fastgpt-iframe";
-  script.setAttribute("data-src", "https://fastgpt.run/chat/share?shareId=5nps718vnusl8oo5zbv9pa7y");
-  script.setAttribute("data-color", "#4e83fd");
-  document.body.appendChild(script);
-}
 
-// 调用函数以嵌入 Fast GPT
-embedFastGPT();
 
 
 
